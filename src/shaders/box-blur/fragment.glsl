c11ed1b9ad25788b38bd5ea6b2742305e7ebd9bf
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-06-27 11:04:45 +0200

    add vertex uvs

--- src/shaders/box-blur/fragment.glsl
+++ src/shaders/box-blur/fragment.glsl
@@ -3,31 +3,60 @@ uniform int uRadius;
 uniform vec2 uDirection;
 
 varying vec2 vUv;
+varying float VNormalization;
+varying vec4 vUv0;
+varying vec4 vUv1;
+varying vec4 vUv2;
+varying vec4 vUv3;
+varying vec4 vUv4;
+varying vec4 vUv5;
+varying vec4 vUv6;
+varying vec4 vUv7;
+varying vec4 vUv8;
+varying vec4 vUv9;
+varying vec4 vUv10;
 
 void main() {
 
-  ivec2 iRes = textureSize(tDiffuse, 0);
-  vec2 texel = 1.0 / vec2(iRes);
+  // ivec2 iRes = textureSize(tDiffuse, 0);
+  // vec2 texel = 1.0 / vec2(iRes);
   vec3 diffuse = vec3(0.0);
-  float normalization = 0.0;
-  vec2 direction = uDirection;
+  // float normalization = 0.0;
+  // vec2 direction = uDirection;
 
-  // add box blur 5x5
-  // complextity O(n^2). Let's imrpove this!
-  for(int x = -uRadius; x <= uRadius; x++) {
-    // for(int y = -uRadius; y <= uRadius; y++) {
-      vec2 uv = vUv + float(x) * texel * direction;
-      vec3 c = texture(tDiffuse, uv).rgb;
-      diffuse += c;
-      normalization += 1.0;
-    // }
-  }
+  // // add box blur 5x5
+  // // complextity O(n^2). Let's imrpove this!
+  // for(int x = -uRadius; x <= uRadius; x++) {
+  //   // for(int y = -uRadius; y <= uRadius; y++) {
+  //     vec2 uv = vUv + float(x) * texel * direction;
+  //     vec3 c = texture(tDiffuse, uv).rgb;
+  //     diffuse += c;
+  //     normalization += 1.0;
+  //   // }
+  // }
+  diffuse += texture(tDiffuse, vUv0.xy).rgb;
+  diffuse += texture(tDiffuse, vUv1.xy).rgb;
+  diffuse += texture(tDiffuse, vUv2.xy).rgb;
+  diffuse += texture(tDiffuse, vUv3.xy).rgb;
+  diffuse += texture(tDiffuse, vUv4.xy).rgb;
+  diffuse += texture(tDiffuse, vUv5.xy).rgb;
+  diffuse += texture(tDiffuse, vUv6.xy).rgb;
+  diffuse += texture(tDiffuse, vUv7.xy).rgb;
+  diffuse += texture(tDiffuse, vUv8.xy).rgb;
+  diffuse += texture(tDiffuse, vUv9.xy).rgb;
+  diffuse += texture(tDiffuse, vUv10.xy).rgb;
 
-  diffuse /= normalization;
-
-  if(vUv.x < 0.5) {
-    diffuse = texture(tDiffuse, vUv).rgb;
-  }
+  diffuse += texture(tDiffuse, vUv1.zw).rgb;
+  diffuse += texture(tDiffuse, vUv2.zw).rgb;
+  diffuse += texture(tDiffuse, vUv3.zw).rgb;
+  diffuse += texture(tDiffuse, vUv4.zw).rgb;
+  diffuse += texture(tDiffuse, vUv5.zw).rgb;
+  diffuse += texture(tDiffuse, vUv6.zw).rgb;
+  diffuse += texture(tDiffuse, vUv7.zw).rgb;
+  diffuse += texture(tDiffuse, vUv8.zw).rgb;
+  diffuse += texture(tDiffuse, vUv9.zw).rgb;
+  diffuse += texture(tDiffuse, vUv10.zw).rgb;
+  diffuse /= 21.;
 
   gl_FragColor = vec4(diffuse, 1.0);
 
--- src/shaders/box-blur/vertex.glsl
+++ src/shaders/box-blur/vertex.glsl
@@ -1,6 +1,54 @@
+uniform sampler2D tDiffuse;
+uniform int uRadius;
+uniform vec2 uDirection;
+
 varying vec2 vUv;
+varying float VNormalization;
+varying vec4 vUv0;
+varying vec4 vUv1;
+varying vec4 vUv2;
+varying vec4 vUv3;
+varying vec4 vUv4;
+varying vec4 vUv5;
+varying vec4 vUv6;
+varying vec4 vUv7;
+varying vec4 vUv8;
+varying vec4 vUv9;
+varying vec4 vUv10;
 
 void main() {
+
   vUv = uv;
+
+  ivec2 iRes = textureSize(tDiffuse, 0);
+  vec2 texel = 1.0 / vec2(iRes);
+  vec3 diffuse = vec3(0.0);
+  float normalization = 0.0;
+  vec2 direction = uDirection;
+
+  vUv0.xy = uv + texel * uDirection * 0.0;
+  vUv1.xy = uv + texel * uDirection * 1.0;
+  vUv1.zw = uv + texel * uDirection * -1.0;
+  vUv2.xy = uv + texel * uDirection * 2.0;
+  vUv2.zw = uv + texel * uDirection * -2.0;
+  vUv3.xy = uv + texel * uDirection * 3.0;
+  vUv3.zw = uv + texel * uDirection * -3.0;
+  vUv4.xy = uv + texel * uDirection * 4.0;
+  vUv4.zw = uv + texel * uDirection * -4.0;
+  vUv5.xy = uv + texel * uDirection * 5.0;
+  vUv5.zw = uv + texel * uDirection * -5.0;
+  vUv6.xy = uv + texel * uDirection * 6.0;
+  vUv6.zw = uv + texel * uDirection * -6.0;
+   vUv7.xy = uv + texel * uDirection * 7.0;
+  vUv7.zw = uv + texel * uDirection * -7.0;
+
+   vUv8.xy = uv + texel * uDirection * 8.0;
+  vUv8.zw = uv + texel * uDirection * -8.0;
+
+   vUv9.xy = uv + texel * uDirection * 9.0;
+  vUv9.zw = uv + texel * uDirection * -9.0;
+
+   vUv10.xy = uv + texel * uDirection * 10.0;
+  vUv10.zw = uv + texel * uDirection * -10.0;
   gl_Position = vec4(position, 1.0);
 }
