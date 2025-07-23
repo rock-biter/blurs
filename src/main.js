d3582af7cfd24b4971c0537cd3506c03cf9c7ab5
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-07-23 09:57:58 +0200

    change camera position

--- src/main.js
+++ src/main.js
@@ -68,7 +68,7 @@ const sizes = {
  */
 const fov = 60
 const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.1)
-camera.position.set(2, 2, 2)
+camera.position.set(2.5, 2.5, 2.5)
 camera.lookAt(new THREE.Vector3(0, 2.5, 0))
 
 /**
